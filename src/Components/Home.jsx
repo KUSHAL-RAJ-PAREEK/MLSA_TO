b43af36b1e30fc2f991c276b549c768c8fb158ae
KUSHAL RAJ PAREEK <115624967+KUSHAL-RAJ-PAREEK@users.noreply.github.com>
2023-12-20 15:56:46 +0530

    commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/ME1.jpg";
+import image from "../images/KRP_photo.jpg";
 
 const imageAltText = "Kushal raj pareek Background image";
 
